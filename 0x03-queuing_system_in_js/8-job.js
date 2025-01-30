fb3a5bd0d888b900e30563dfe6ece5c8442fb4f2
Abdelhady117 <111305193+Abdelhady117@users.noreply.github.com>
2025-01-31 01:21:20 +0200

    Create 8-job.js

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,16 @@
+export default function createPushNotificationJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+  jobs.forEach(jobData => {
+    const job = queue.create('push_notification_code_3', jobData);
+
+    job.save((err) => {
+      if (!err) {
+        console.log(`Notification job created: ${job.id}`);
+      } else {
+        console.error(`Notification job failed: ${job.id}`);
+      }
+    });
+  });
+}
